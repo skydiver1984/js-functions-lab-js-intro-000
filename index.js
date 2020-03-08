64caafd301347573f829190c889fd5e11580d76e
Luis Hawkins <bladerunnerluis@gmail.com>
2020-03-08 02:47:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyCustomHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's ${holiday} days until ${name}!`
+  return `It's ${days} days until ${holiday}!`
 }
