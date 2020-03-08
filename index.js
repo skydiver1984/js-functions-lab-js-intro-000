4f716286d5e27e2620726f675f51b87521940646
Luis Hawkins <bladerunnerluis@gmail.com>
2020-03-08 02:41:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-funtion happyCustomHolidayTo(holiday, name) {
+function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
